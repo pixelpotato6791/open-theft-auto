bb5e9f163339a75cdc67fdf30633ae50924484b2
Adam <rawritsadam90@gmail.com>
2022-09-26 17:29:01 +0100

    added dev scnee

--- assets/weapons/Weapon.gd
+++ assets/weapons/Weapon.gd
@@ -4,10 +4,13 @@ extends Node2D
 class_name Weapon
 
 export(PackedScene) var Bullet
-export(int) var 		damage
-export(bool) var  		ismelee
-export(int) var   		maxammo
-export(float) var 		distance
+export(int)   		var damage
+export(bool)  		var ismelee
+export(int)   		var maxammo
+export(float) 		var distance
+
+func _ready():
+	Bullet = preload("res://assets/weapons/Bullet.tscn")
 
 func shoot():
 	if ismelee:
