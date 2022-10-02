571f3dddda9ff9bc8d62a8043608dd804d66ac1b
Adam <rawritsadam90@gmail.com>
2022-10-02 22:17:00 +0100

    fixed health script

--- assets/helpers/Health.gd
+++ assets/helpers/Health.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-export(int) var health
+export(int) var health = 100
 
 signal health_changed(old_value, new_value)
 
 func take_damage(x:int):
-	if health > 0: return
+	if health <= 0: return
 
 	var old_health = health
 	health -= x
