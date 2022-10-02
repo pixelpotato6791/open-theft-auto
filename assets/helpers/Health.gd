99fc9f56cb83fd7e470e55aca93fce9871838fa9
Adam <rawritsadam90@gmail.com>
2022-10-02 22:11:16 +0100

    added if statement if health is zero in health

--- assets/helpers/Health.gd
+++ assets/helpers/Health.gd
@@ -5,6 +5,8 @@ export(int) var health
 signal health_changed(old_value, new_value)
 
 func take_damage(x:int):
+	if health > 0: return
+
 	var old_health = health
 	health -= x
 	emit_signal("health_changed", old_health, health)
