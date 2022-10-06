a0fa2a1c377f561d3fb837cb1cf326c181a5dd04
Adam <rawritsadam90@gmail.com>
2022-10-06 21:38:39 +0100

    added a comment to game.gd

--- game.gd
+++ game.gd
@@ -4,5 +4,8 @@ export(PackedScene) var player
 
 export(String) var PLAYER_GROUP_TAG = "Player"
 
+# might need to remove this since the org_obj might not be a Position2D
+# cant always use the same code so might need a switch case for the type
+# of org_obj. Am i making sense? oh well life goes on ay
 func distance_to_player(org_obj:Position2D):
 	return int(org_obj.position.distance_to(player.global_position))
